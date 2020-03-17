38ef5c6e34e83f4bbcb702e80ef854e8a6f127d6
zwyak <vlad.senyuk97@gmail.com>
2020-03-17 10:40:51 +0300

    Updated files

--- animate.js
+++ animate.js
@@ -1,4 +1,6 @@
 var svg = document.getElementById("svgMap");
+var coords = [];
+var anim = true;
 
 var svgimg = document.createElementNS('http://www.w3.org/2000/svg','image');
 svgimg.setAttributeNS(null,'id','catAnim');
@@ -20,10 +22,20 @@ var direction = true;
 var counter = 0;
 var ct = cat.offsetTop, cl = cat.offsetLeft;
 
+function stopAnim(){
+	anim = false;
+	if (anim) requestAnimationFrame(myMove.moveStar);
+}
+
+function nextAnim(){
+	anim = true;
+	if (anim) requestAnimationFrame(myMove.moveStar);
+}
 
 function createCurve(){
 	for (var i = 0; i < paths.length; i++) {
 		points += paths[i].getPointAtLength(0).x + ',' +  paths[i].getPointAtLength(0).y + " ";
+		coords.push({x: paths[i].getPointAtLength(0).x, y: paths[i].getPointAtLength(0).y, data: paths[i].getAttribute('data-tooltip')});
 	}
 	document.getElementById('polyline').setAttribute('points', points);
 }
@@ -34,7 +46,15 @@ var curveLength = svg.getElementById('polyline').getTotalLength()
 
 function myMove() {
 
-	function moveStar() {
+	$('.start-butt').prop('disabled', true);
+
+	function pause(){
+	    anim = true;
+			$(".discr").fadeOut();
+			if (anim) requestAnimationFrame(moveStar);
+	}
+
+ function moveStar() {
 	if (parseInt(counter, 10) === 1) {
 		direction = false;
 	} else if (parseInt(counter, 10) < 0) {
@@ -47,11 +67,30 @@ function myMove() {
 		counter -= 0.003;
 	}
 
-	cat.setAttribute("transform","translate("+ (svg.getElementById('polyline').getPointAtLength(counter * curveLength).x - 35)  + "," + (svg.getElementById('polyline').getPointAtLength(counter * curveLength).y -15) + ")");
-	requestAnimationFrame(moveStar);
+	var p1 = svg.getElementById('polyline').getPointAtLength(counter * curveLength).x - 35;
+	var p2 = svg.getElementById('polyline').getPointAtLength(counter * curveLength).y - 15;
+	cat.setAttribute("transform","translate("+ (p1)  + "," + (p2) + ")");
+
+	for (var i = 0; i < coords.length; i++) {
+		if (Math.round(getTranslate(cat).x) == (Math.round(coords[i].x) - 35) &&  Math.round(getTranslate(cat).y) == (Math.round(coords[i].y) - 15)){
+			anim = false;
+			$(".discr").html(coords[i].data);
+			$(".discr").fadeIn();
+			setTimeout(pause, 2000);
+		}
+	}
+
+	if (anim) requestAnimationFrame(moveStar);
+}
+myMove.moveStar = moveStar;
+	if (anim) requestAnimationFrame(moveStar);
 }
 
-	requestAnimationFrame(moveStar);
+
+function getTranslate(myElement) {
+  var style = window.getComputedStyle(myElement);
+  var matrix = new WebKitCSSMatrix(style.webkitTransform);
+  return { x: matrix.m41, y: matrix.m42 };
 }
 
 // var myProcess;
